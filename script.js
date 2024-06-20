2c11a95635dd5b9d5897dfd7e8322b1abd1ffb7a
MihaiMariusCatalin <167853612+MihaiMariusCatalin@users.noreply.github.com>
2024-06-20 13:52:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,39 @@
-var word
+let words = ['portocala', 'mere', 'para', 'banana', 'ananas']
+let word = words[Math.floor(Math.random() * words.length)]
+document.getElementById('letters').innerText = word.length
 
-function generate() {
-    word = document.getElementById('start-input').value
-    document.getElementById("size").innerText = word.length
-    for (let i = 0; i < word.length; ++i) {
-        let letter = document.createElement("span")
-        letter.id = i
-        letter.innerText = "_"
-        document.getElementById("start-word").append(letter)
-    }
-    document.getElementById('start-input').value = ""
+for (let i = 0; i < word.length; ++i) {
+    let letter = document.createElement('span')
+    letter.id = i
+    letter.innerText = '_'
+    document.getElementById('text').append(letter)
 }
 
-let correct = 0
+let cnt = 0;
 
-function guess() {
-    let letter = document.getElementById("guess-input").value
-    let wrong = 1
+function check() {
+    let letter = document.getElementById('input').value
+    let flag = 1
     for (let i = 0; i < word.length; ++i) {
-        if (letter == word[i]) {
+        if (letter == word[i] && document.getElementById(i).innerText != word[i]) {
             document.getElementById(i).innerText = letter
-            wrong = 0
-            ++correct
+            ++cnt
+            flag = 0
+            console.log(cnt)
         }
     }
-    if (wrong == 1) {
+    document.getElementById('input').value = ''
+    if (flag == 1) {
         document.getElementById('lives').innerText -= 1
     }
-    document.getElementById('guess-input').value = ""
+    if (cnt == word.length) {
+        let finish = document.createElement('h1')
+        finish.innerText = "Congratulation, you win!"
+        document.getElementById('mainDiv').append(finish)
+    }
     if (document.getElementById('lives').innerText == 0) {
         let finish = document.createElement('h1')
-        finish.innerText = "Ai pierdut"
-        document.getElementById('start-div').append(finish)
-    }
-    if (correct == word.length) {
-        let finish = document.createElement('h1')
-        finish.innerText = "Ai castigat"
-        document.getElementById('start-div').append(finish)
-    }
+        finish.innerText = "You lose!"
+        document.getElementById('mainDiv').append(finish) 
+    } 
 }
